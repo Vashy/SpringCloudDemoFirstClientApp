71de523aeb85e4586f27f2a0172c46162d453450
Timoty Granziero <timoty.granziero@arcavita.it>
2021-06-08 19:25:20 +0200

    Initial commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,10 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.springframework.cloud:spring-cloud-starter-config")
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+	implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
--- src/main/kotlin/it/vashykator/spring/cloud/app/first/FirstClientAppApplication.kt
+++ src/main/kotlin/it/vashykator/spring/cloud/app/first/FirstClientAppApplication.kt
@@ -2,10 +2,31 @@ package it.vashykator.spring.cloud.app.first
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
 class FirstClientAppApplication
 
 fun main(args: Array<String>) {
 	runApplication<FirstClientAppApplication>(*args)
+}
+
+@RestController
+class Controller {
+	@GetMapping("/aooo")
+	fun a() = "Value!"
+}
+
+@FeignClient("spring-cloud-eureka-client")
+interface GreetingClient {
+	@RequestMapping("/greeting")
+	fun greeting(): String
 }
